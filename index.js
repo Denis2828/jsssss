dd31af950245b37b15e5306c708a15c46f865448
Denis2828 <60971722+Denis2828@users.noreply.github.com>
2020-02-13 13:18:39 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -11,15 +11,38 @@ bot.setWebHook(`${url}/bot${TOKEN}`);
 
 var USER_NAME = "";
 
+function send(message){
+	bot.sendMessage(msg.chat.id, message);
+}
+
+var commands = [
+	{"input": "Привет", "output": "Привет, друг)"},
+	{"input": "Пока", "output": "До встречи!"},
+	{"input": "Как дела?", "output": "Отлично!"},
+	{"input": "Что делаешь?", "output": "Общаюсь с тобой)"},
+	{"input": "Как тебя зовут?", "output": "Меня зовут Bot_Den, для друзей просто Den"}
+]
 
 bot.on('message', function onMessage(msg) {
 	if(msg.text[0] == '/'){
 		var defis = msg.text.indexOf("-");
 		switch(msg.text.slice(1, defis)){
 			case "Меня зовут":
-				USER_NAME = msg.text.slice(defis+2, msg.text.length);
-				bot.sendMessage(msg.chat.id, USER_NAME);
+				USER_NAME = msg.text.slice(defis+1, msg.text.length);
 			break;
+			case "Команды":
+				var list;
+				for(var q=0;q<commands.length;q++){
+					list+=commands[q].input+"\n";
+				}
+				list+="/Меня зовут-[Ваше имя]\n"
+				send(list);
+			break;
+		}
+	}
+	for(var i = 0;i<commands.length;i++){
+		if(msg.text == commands[i].input){
+			send(commands[i].output);
 		}
 	}
 });
